7e589731fce16195993bbc7c5a2ce6625f121c12
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-10-26 17:09:44 +0800

    add conditional rendering
    add conditional rendering

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -12,14 +12,28 @@ const Cards = ({ car }) => {
             : "bg-veryDarkCyan rounded-bl-xl rounded-br-xl md:rounded-bl-none md:rounded-tr-xl"
         }`}
       >
-        <div className="w-full ">
-        <img src={car.icon} className="w-20" />
+        <div className="w-full">
+          <img src={car.icon} className="w-20" />
         </div>
         <div className="my-6 w-52">
-          <h1 className="font-headerFont py-6 text-5xl">{car.title}</h1>
-          <p className="font-sans py-2">{car.body}</p>
+          <h1 className=" text-transparentWhite font-headerFont py-6 text-5xl">
+            {car.title}
+          </h1>
+          <p className=" text-transparentWhite font-sans py-2">{car.body}</p>
         </div>
-        <button className="rounded-full py-4 px-7">Learn More</button>
+        <button
+          className={`${
+            car.id === 1
+              ? "text-brightOrange bg-white rounded-3xl px-4 py-2"
+              : 
+            car.id === 2 
+              ? "text-darkCyan bg-white rounded-3xl px-4 py-2"
+              :
+              "text-veryDarkCyan bg-white rounded-3xl px-4 py-2"
+          }`}
+        >
+          Learn More
+        </button>
       </div>
     </div>
   );
