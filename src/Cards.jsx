370e6266dff5794a9edb611e14659cd8ae34f300
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-10-27 10:26:48 +0800

    Update Cards.jsx
    Update Cards.jsx

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from "react";
 
 const Cards = ({ car }) => {
+
+
+
   return (
-    <div>
+    <div className="w-10/12 md:w-auto">
       <div
-        className={`${
+        className={`${ 
           car.id === 1
-            ? "bg-brightOrange rounded-tl-xl rounded-tr-xl rounded-bl-none md:rounded-tr-none md:rounded-bl-xl "
+            ? "bg-brightOrange p-12 rounded-tl-md rounded-tr-md rounded-bl-none md:rounded-tr-none md:rounded-bl-xl "
             : car.id === 2
-            ? "bg-darkCyan"
-            : "bg-veryDarkCyan rounded-bl-xl rounded-br-xl md:rounded-bl-none md:rounded-tr-xl"
+            ? "bg-darkCyan p-12 "
+            : "bg-veryDarkCyan p-12  rounded-bl-md rounded-br-md md:rounded-bl-none md:rounded-tr-xl"
         }`}
       >
-        <div className="w-full">
+        <div className="w-full ">
           <img src={car.icon} className="w-20" />
         </div>
         <div className="my-6 w-52">
@@ -24,12 +27,12 @@ const Cards = ({ car }) => {
         <button
           className={`${
             car.id === 1
-              ? "text-brightOrange bg-white rounded-3xl px-4 py-2"
+              ? "text-brightOrange border-2 border-white bg-white rounded-3xl px-4 py-2 hover:bg-brightOrange hover:border-2 hover:border-white hover:text-white"
               : 
             car.id === 2 
-              ? "text-darkCyan bg-white rounded-3xl px-4 py-2"
+              ? "text-darkCyan border-2 border-white bg-white rounded-3xl px-4 py-2 hover:bg-darkCyan hover:border-2 hover:border-white hover:text-white"
               :
-              "text-veryDarkCyan bg-white rounded-3xl px-4 py-2"
+              "text-veryDarkCyan border-2 border-white bg-white rounded-3xl px-4 py-2 hover:bg-veryDarkCyan hover:border-2 hover:border-white hover:text-white"
           }`}
         >
           Learn More
